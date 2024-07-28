0337d932cac15ec55decd73cdd9ab61fbb0e4462
Moses361 <odhismoses20@gmail.com>
2024-07-28 08:29:20 +0300

    remove unnecessary elements

--- includes/footer.php
+++ includes/footer.php
@@ -1,113 +1,54 @@
 <div id="footer"><!-- footer begin-->
     <div class="container"><!-- container begin-->
         <div class="row"><!-- row begin-->
-            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 begin-->
-                 <h4>Pages</h4>
-             <ul><!-- ul begin-->
-                 <li><a href="bookingHistory.php">Order Cart</a></li>
-                 <li><a href="contact.php">Contac Us</a></li>
-                 <li><a href="shop.php">Order</a></li>
-                 <li><a href="customer/my_account.php">My Account</a></li>
+            <div class="col-sm-6 col-md-4"><!-- col-sm-6 col-md-3 begin-->
+                 <h4>Quick Links</h4>
+             <ul class="mt-3"><!-- ul begin-->
+                 <li class="pt-2"><a href="index.php">Home</a></li>
+                 <li class="pt-2"><a href="bookingHistory.php">Booking History</a></li>
+                 <li class="pt-2"><a href="contact.php">Contact Us</a></li>
+                 <li class="pt-2"><a href="customer/my_account.php">My Account</a></li>
+                <li class="pt-2"><a href="terms.php">Terms & Conditions</a></li>
              </ul><!-- ul Finish-->
-            <hr>
-             <h4>User Section</h4>
-               <ul><!-- ul begin-->
-                   <?php 
-                                       
-                        if(!isset($_SESSION['customer_email'])){
-
-                            echo"<a href='login.php'>Login</a>";
-                            }else{
-
-                                 echo"<a href='customer/my_account.php?my_orders'>MY ACCOUNT</a>";
-
-                            }
-                                       
-                    ?>
-                   <li><a href="customer_register.php">Register</a></li>
-                    <li><a href="terms.php">Terms & Conditions</a></li>
-               </ul><!-- ul Finish-->
-               <hr class="hidden-md hidden-lg hidden-sm">
             </div><!-- col-sm-6 col-md-3 Finish-->
-            <div class="com-sm-6 col-md-3"><!-- com-sm-6 col-md-3 begin-->
-            
-             <h4>Top Services Categories</h4>
-                <ul><!-- ul begin-->
-                    <?php 
-                    
-                       $get_p_cats = "select * from product_categories";
-                       $run_p_cats = mysqli_query($con,$get_p_cats);
-
-                       while($row_p_cats=mysqli_fetch_array($run_p_cats)){
-
-                          $p_cat_id = $row_p_cats['p_cat_id'];
-                          $p_cat_title = $row_p_cats['p_cat_title'];
-                          
-                            echo"
-                            
-                             <li>
-                             
-                                 <a href='shop.php?p_cats=$p_cat_id'>
-                                 
-                                     $p_cat_title 
-                                 
-                                 </a>
-                             
-                             </li>
-                            
-                            
-                            ";
-                             
-
-                       }
-                    
-                    ?>
-                </ul><!-- ul Finish-->
-             <hr class="hidden-md hidden-lg">
-
-            </div><!-- com-sm-6 col-md-3 Finish-->
-            <div class="com-sm-6 col-md-3"><!-- com-sm-6 col-md-3 begin-->
+            <div class="com-sm-6 col-md-4"><!-- com-sm-6 col-md-4 begin-->
                 <h4>Find Us</h4>
-                <p><!-- p begin-->
-                  <strong>Real Estate Investements.</strong>
-                  </br>Nairobi
-                  </br>Tel
-                  </br>0741261053
-                  </br>missmalika@gmail.com
-                  </br><strong>Miss Malika</strong>
-                  </br>
-                </p><!-- p Finish-->
+                <ul class="mt-3">
+                    <li class="pt-2">
+                        <a href="#"> <i class="fa fa-map-marker"></i> Malika Properties Ltd. Westlands, Nairobi</a>
+                    </li>
+                    <li class="pt-2">
+                        <a href="#"><i class="fa fa-phone"></i> 0741261053</a>
+                    </li>
+                    <li class="pt-2">
+                        <a href="#"><i class="fa fa-envelope"></i> missmalika@gmail.com </a>
+                    </li>
+                </ul>
+            </div><!-- com-sm-6 col-md-4 Finish-->
 
-                <a href="contact.php">Check Our Contact Page</a>
-                <hr class="hidden-md hidden-lg">
-            </div><!-- com-sm-6 col-md-3 Finish-->
-            <div class="col-sm-6 col-md-3"></div><!-- col-sm-6 col-md-3 begin-->
+            <div class="col-sm-6 col-md-4">
              <h4>Get The News</h4>
-             <p class="text-muted">
-                Dont miss our Latest updated Service
-             </p>
-             <form action="" method="post"><!-- form begin-->
+             <form action="" method="post" class="mt-3 w-3/4"><!-- form begin-->
                  <div class="input-group"><!-- input-group begin-->
-                 
-                 <input type="text" class="form-control" name="email">
-                 <span class="input-group-btn "><!-- span input-group-btn begin-->
-                       <input type="submit" value="subscribe" class="btn btn-danger"> 
-
-
-                 </span><!-- span input-group-btn Finish-->
-
+                    <input type="text" class="form-control" name="email" placeholder="Enter your Email">
+                    <span class="input-group-btn "><!-- span input-group-btn begin-->
+                        <input type="submit" value="Subscribe"  class="btn bg-primary"> 
+                    </span><!-- span input-group-btn Finish-->
                  </div><!-- input-group Finish-->
-                 
              </form><!-- form Finish-->
-             <hr>
-             <h4>Keep In Touch</h4>
-             <p class="social">
-                 <a href="#" class="fa fa-facebook"></a>
-                 <a href="#" class="fa fa-twitter"></a>
-                 <a href="#" class="fa fa-instagram"></a>
-                 <a href="#" class="fa fa-google-plus"></a>
-                 <a href="#" class="fa fa-envelope"></a>
-             </p>
+             
+             <h4 class="mt-5">Keep In Touch</h4>
+             <ul class="mt-3">
+                <li class="mt-2">
+                    <a href="#"><i class="fa fa-facebook bg-gray-400 text-white rounded-full p-3 px-4"></i><span class="text-black" > MalikaProperties </span> </a>
+                </li>
+                <li class="mt-2">
+                    <a href="#" ><i class="fa fa-twitter bg-gray-400 text-white rounded-full p-3"></i>  <span class="text-black" >@MalikaProperties </span> </a>
+                </li>
+                <li class="mt-2">
+                    <a href="#"><i class="fa fa-instagram bg-gray-400 text-white rounded-full p-3"></i>  <span class="text-black" >@MalikaPropertiesKE </span> </a>
+                </li>
+            </ul>
             </div><!-- col-sm-6 col-md-3 Finish-->
         </div><!-- row Finish-->
     </div><!-- container Finish-->
@@ -116,10 +57,10 @@
 <div id="copyright"><!-- copyright begin-->
     <div class="container"><!-- container begin-->
         <div class="col-md-6"><!-- col-md-6 begin-->
-            <p class="pull-left">&copy;2024 Real Estate Investements All Rights reserved</p>
+            <p class="pull-left">&copy;2024 Malika Properties Ltd. All Rights Reserved</p>
         </div><!-- col-md-6 Finish-->
          <div class="col-md-6"><!-- col-md-6 begin-->
-            <p class="pull-right"> Designed and Managed by: missmalika</p>
+            <p class="pull-right"> Designed and Managed by MissMaliaka</p>
         </div><!-- col-md-6 Finish-->
     </div><!-- container Finish-->
 </div><!-- copyright Finish-->
