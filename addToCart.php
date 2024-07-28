d4c39970f13b573c1b025b403e48ae87a90dd2c1
Moses361 <odhismoses20@gmail.com>
2024-07-28 08:28:38 +0300

    capture new booking info

--- addToCart.php
+++ addToCart.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 include("functions/functions.php");
 // process order
 if (!isset($_POST['addToCart'])) {
@@ -8,32 +10,29 @@ if (!isset($_POST['addToCart'])) {
 $db = mysqli_connect("localhost", "root", "", "pam");
 $ip_add = getRealIpUser();
 
-// for testing 
-header("Location: checkout.php");
-exit();
-    
 $p_id = $_POST['product_id'];
-
-$product_qty = @$_POST['product_qty'];
-
-$product_size = @$_POST['product_size'];
-
+$base_price = $_POST['base_price'];
+$num_units = @$_POST['num_units'];
 $total = $_POST['total'];
 
 $check_product = "select * from cart where ip_add='$ip_add' AND p_id='$p_id'";
 
 $run_check = mysqli_query($db, $check_product);
 
-if (mysqli_num_rows($run_check) > 0) {
+// set session for the total amount the customer is to pay
+$_SESSION['booking']['total'] = $base_price * $num_units;
 
-    echo "<script>alert('This product has already been added to cart')</script>";
-    echo "<script>window.open('details.php?pro_id=$p_id','_self')</script>";
+header("Location: checkout.php");
+// if (mysqli_num_rows($run_check) > 0) {
 
-} else {
+//     echo "<script>alert('This product has already been added to cart')</script>";
+//     echo "<script>window.open('details.php?pro_id=$p_id','_self')</script>";
 
-    $query = "insert into cart (p_id,ip_add,qty,size, p_price) values ('$p_id','$ip_add','$product_qty','$product_size', '$total')";
-    $run_query = @mysqli_query($db, $query);
+// } else {
+
+//     $query = "insert into cart (p_id,ip_add,qty,size, p_price) values ('$p_id','$ip_add','$product_qty','$product_size', '$total')";
+//     $run_query = @mysqli_query($db, $query);
     
-    echo "<script>window.open('details.php?pro_id=$p_id','_self')</script>";
+//     echo "<script>window.open('details.php?pro_id=$p_id','_self')</script>";
 
-}
+// }
